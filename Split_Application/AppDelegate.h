bcca574278685757afa7fcd5251c7c3142a94b92
Tarek Qawasmi <tarek@Tareks-Mac.local>
2017-11-25 11:38:55 +0000

    Initial Commit

---
+++ Split_Application/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Split_Application
+//
+//  Created by Tarek Qawasmi on 25/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Split_Application/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Split_Application
+//
+//  Created by Tarek Qawasmi on 25/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
