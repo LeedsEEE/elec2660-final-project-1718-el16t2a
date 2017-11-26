c46778aa531f3b27737c77bf0dc513ffed970792
Tarek Qawasmi <tarek@Tareks-Mac.local>
2017-11-26 18:05:04 +0000

    creating a new class called MenuElement which will have the details of each element such as, it's price, it's ingredients, it's name and it's type

---
+++ Split_Application/MenuElement.h
@@ -0,0 +1,13 @@
+//
+//  MenuElement.h
+//  Split_Application
+//
+//  Created by Tarek Qawasmi on 26/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MenuElement : NSObject
+
+@end
---
+++ Split_Application/MenuTableTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuTableTableViewController.h
+//  Split_Application
+//
+//  Created by Tarek Qawasmi on 26/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuTableTableViewController : UITableViewController
+
+@end
