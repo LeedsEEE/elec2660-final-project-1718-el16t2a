ca0a16c8579547a98141502456659b32f44d4e69
Tarek Qawasmi <tarek@Tareks-Mac.local>
2017-11-26 22:45:43 +0000

    Table View should be done, but it's not working. Code needs debugging

---
+++ delete me.m
@@ -0,0 +1,13 @@
+//
+//  delete me.m
+//  Split_Application
+//
+//  Created by Tarek Qawasmi on 26/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import "delete me.h"
+
+@implementation delete_me
+
+@end
