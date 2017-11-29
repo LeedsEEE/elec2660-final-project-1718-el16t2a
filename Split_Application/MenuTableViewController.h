6e2aae9d7c2ca808af7d8e6d50bb88666d1a66b4
Tarek Qawasmi <tarek@Tareks-Mac.local>
2017-11-29 20:00:39 +0000

    as part of debugging, we'll start the table view from scratch

---
+++ Split_Application/MenuTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuTableViewController.h
+//  Split_Application
+//
+//  Created by Tarek Qawasmi on 29/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuTableViewController : UITableViewController
+
+@end
--- Split_Application/MenuTableViewController.m
+++ Split_Application/MenuTableViewController.m
@@ -1,23 +1,21 @@
 //
-//  MenuTableTableViewController.m
+//  MenuTableViewController.m
 //  Split_Application
 //
-//  Created by Tarek Qawasmi on 26/11/2017.
+//  Created by Tarek Qawasmi on 29/11/2017.
 //  Copyright © 2017 University of Leeds. All rights reserved.
 //
 
-#import "MenuTableTableViewController.h"
+#import "MenuTableViewController.h"
 
-@interface MenuTableTableViewController ()
+@interface MenuTableViewController ()
 
 @end
 
-@implementation MenuTableTableViewController
+@implementation MenuTableViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    self.data = [[MenuDataModel alloc] init];
     
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
@@ -34,52 +32,24 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-
-    return 2; //to be 6 later
+#warning Incomplete implementation, return the number of sections
+    return 0;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-   
-    NSInteger  numberOfRows;
-
-    if(section == 0) {
-        numberOfRows= self.data.AppetizerElement.count ;
-    }
-    
-    if(section == 1) {
-        numberOfRows= self.data.AppetizerElement.count ;
-    }
-    
-    
-    return  numberOfRows;
+#warning Incomplete implementation, return the number of rows
+    return 0;
 }
 
--(NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section {
-   
-    return [NSString stringWithFormat:@"Level %ld Modules",section+1];
-}
-
+/*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MenuElementCell" forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
     
     // Configure the cell...
- 
-    // We create a temporary model object to have the information from MenuDataModel while we add it to the cell
     
-    if (indexPath.section == 0 ) /* appetizer correspond to section 0 */ {
-        MenuElement*tempMenuElement = [self.data.AppetizerElement objectAtIndex:indexPath.row];
- 
-        cell.textLabel.text=tempMenuElement.Name;
-    }
-    
-    if (indexPath.section == 1 ) /*  correspond to section 1 */ {
-        MenuElement*tempMenuElement = [self.data.BurgerElement objectAtIndex:indexPath.row];
-        
-        cell.textLabel.text=tempMenuElement.Name;
-    }
     return cell;
 }
-
+*/
 
 /*
 // Override to support conditional editing of the table view.
